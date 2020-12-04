a0fe57fbc80d3c9b2adb4d28bb58cab82c79ddad
Tanner <dolb.tanner@gmail.com>
2020-12-03 22:46:12 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2020
+My journey attempting the challenges in the [Advent of Code](https://adventofcode.com/)
---
+++ logic/day-one.js
@@ -0,0 +1,270 @@
+/* 
+    Day 1: Find the two entries that sum to 2020; what do you get if you multiply them together?
+*/
+
+const input = 
+`1078
+1109
+1702
+1293
+1541
+1422
+1679
+1891
+1898
+1455
+1540
+1205
+1971
+1582
+1139
+1438
+1457
+1725
+1907
+1872
+1101
+1403
+1557
+1597
+1619
+1974
+1287
+292
+1647
+1444
+1241
+879
+1761
+1067
+1178
+1510
+1110
+1233
+1121
+1299
+1796
+1124
+1768
+1466
+1871
+1279
+1344
+1485
+1258
+1179
+1147
+492
+1234
+1843
+1421
+1819
+1964
+1671
+1793
+1302
+1731
+1886
+1686
+1150
+1806
+1960
+1841
+1936
+1845
+1520
+1779
+1102
+1323
+1892
+1742
+1941
+1395
+1525
+1165
+715
+1829
+1448
+1906
+1191
+1981
+1115
+1716
+1644
+1310
+1836
+1105
+1517
+1790
+1950
+1741
+1256
+1467
+1677
+1372
+1838
+1637
+1143
+1763
+1222
+1291
+1835
+1602
+1927
+1933
+1952
+1692
+1662
+1967
+1791
+1984
+1176
+1324
+1460
+1416
+562
+1862
+1273
+1518
+1535
+1093
+1977
+1923
+1246
+1570
+1674
+1861
+1811
+1431
+47
+1158
+1912
+1322
+1062
+1407
+1528
+1068
+1868
+1997
+1930
+959
+1676
+1759
+2000
+1993
+1722
+1738
+1264
+1361
+1542
+1187
+1735
+1405
+1745
+1753
+1833
+1493
+1311
+1547
+1180
+1553
+1513
+1812
+1951
+1948
+1834
+1925
+1726
+1326
+1931
+1962
+1947
+1173
+1633
+1901
+1781
+1483
+1789
+1417
+1929
+1859
+1760
+1347
+1996
+1328
+1798
+1230
+1298
+1877
+1840
+1607
+1253
+1057
+1650
+1171
+1593`;
+
+const inputArr = input.split("\n");
+
+/**
+ * Returns permutations from an array of inputs.
+ * @param {Array} arr The array of input values.
+ * @param {Number} size The size for each permutation
+ */
+function getPermutations(arr, size) {
+   
+    /**
+     * Returns the resulting array of unique pairs
+     * @param {Array} val The array value as a string ie ["1729"]
+     * @param {Number} i The starting point for index variable. 
+     */
+    function c(val, i) {
+        // if permutation size matches size push the pair into resulting arr
+        if (val.length === size) {
+            result.push(val);
+            return;
+        }
+        // if pair item is outside of array bounds
+        if (i + 1 > arr.length) {
+            return; // bail
+        }
+
+        // get permutation pairs
+        let pair = val.concat(arr[i]);
+        c(pair, i + 1); 
+        c(val, i + 1);
+    }
+    var result = [];
+    c([], 0);
+    return result;
+}
+
+const expense_pairs = getPermutations(inputArr, 2);
+const expense_triplets = getPermutations(inputArr, 3);
+
+const dayOnePtOne = document.querySelector(".day-one-pt-one");
+// iterate the expense pairs and find pair that sums to 2020
+expense_pairs.forEach(pair => {
+    let firstNum = parseInt(pair[0]);
+    let secondNum = parseInt(pair[1]);
+    const total = firstNum + secondNum;
+
+    if (total === 2020) {
+        console.log(`Pair found: ${pair[0]} + ${pair[1]} = ${firstNum + secondNum}`);
+        console.log(pair[0] * pair[1]);
+        dayOnePtOne.textContent = `Part One - Pair found: [${firstNum}, ${secondNum}]. Sum equals ${firstNum + secondNum}. Product equals ${firstNum * secondNum}`;
+    }
+});
+
+const dayOnePtTwo = document.querySelector(".day-one-pt-two");
+expense_triplets.forEach(pair => {
+    let firstNum = parseInt(pair[0]);
+    let secondNum = parseInt(pair[1]);
+    let thirdNum = parseInt(pair[2]);
+
+    const total = firstNum + secondNum + thirdNum;
+    if (total === 2020) {
+        console.log(firstNum, secondNum, thirdNum);
+        dayOnePtTwo.textContent = `Part Two - Triplet found: [${firstNum}, ${secondNum}, ${thirdNum}]. Sum equals ${firstNum + secondNum + thirdNum}. Product equals ${firstNum * secondNum * thirdNum}`;
+    }
+})
